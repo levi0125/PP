d96d7fae869187bbfe8b02eb34b7328cd6a2c89f
martin <martnalej@gmail.com>
2025-04-08 22:02:40 -0600

    venv

--- pyweb.py
+++ pyweb.py
@@ -1,17 +1,12 @@
 import webview
+from openFile import openFile
 #apt install python3-webview
 #pip install pywebview
 
 
-html = '''
-<html>
-  <body>
-    <h1>Hola desde HTML</h1>
-  </body>
-</html>
-'''
-
-html=open("./templates/index.html").read()
+#html=open("/home/martin/Desktop/PP/templates/index.html").read()
+html=openFile("./templates/index.html").read()
 
 webview.create_window('Mi App', html=html)
+
 webview.start()
--- readme.md
+++ readme.md
@@ -3,3 +3,11 @@ CREAR ENTORNO VIRTUAL
 python3 -m venv venv
 source venv/bin/activate
 
+CREAR EJECUTABLE:
+
+pyinstaller --onefile --add-data "templates:templates" --add-data "static:static" pyweb.py
+
+ABRIR EJECUTABLE:
+cd dist
+./pyweb
+
